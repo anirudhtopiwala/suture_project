e41c71228c02a8c53e915d828ade3e5e1c912bd4
bharatm11 <bharat.mathur@hotmail.com>
2018-11-15 12:38:19 -0500

    added ellipse corner detection using regionprops

--- suture.m
+++ suture.m
@@ -4,10 +4,25 @@ main_bw=im2bw(main);
 se = strel('line',4,4);
 main_bw = imerode(main_bw,se);
 main_edge = edge(main_bw,'sobel');
+main_edge=bwmorph(main_edge,'thicken');
 main_edge =imfill( main_edge, 'holes' );
+main_edge=bwmorph(main_edge,'thicken');
+main_edge =imfill( main_edge, 'holes' );
+s = regionprops(main_bw,{...
+    'Centroid',...
+    'MajorAxisLength',...
+    'MinorAxisLength',...
+    'Orientation'});
 figure
-imshow(main_edge);
+imshow(main_edge);s.Centroid(1)
 C = detectHarrisFeatures(main_edge,'MinQuality',0.5);
 c= C.Location;
 hold on
-plot(c(:,1),c(:,2),'r*');
+plot(s.Centroid(1),s.Centroid(2),'r*');
+edge_right(1)=(s.Centroid(1)+sind(s.Orientation)*s.MajorAxisLength/4);
+edge_right(2)=s.Centroid(2)+cosd(s.Orientation)*s.MajorAxisLength/4;
+edge_left(1)=(s.Centroid(1)-sind(s.Orientation)*s.MajorAxisLength/4);
+edge_left(2)=s.Centroid(2)+cosd(s.Orientation)*s.MajorAxisLength/4;
+hold on
+plot(edge_right(1),edge_right(2),'g*');
+plot(edge_left(1),edge_left(2),'b*');
